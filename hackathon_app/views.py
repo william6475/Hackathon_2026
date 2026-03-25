2a6b92fad7dfdc2b4e851612f992a2efcc0bdd9e
Atharv-Naphde <atharvrnaphde@gmail.com>
2026-03-25 13:47:52 +0000

    functional views

--- hackathon_app/views.py
+++ hackathon_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def home(request):
+    template = loader.get_template('index.html')
+    temp = "Temp Value"
+    context = {'temp' : temp}
+    return HttpResponse(template.render(context, request))
